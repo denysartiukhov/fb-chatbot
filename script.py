c986b9ccf80302eec63511a3e65cc0ca2407416e
db0ytsov <artiukhov.denys@gmail.com>
2022-08-01 00:06:23 -0500

    add parametrized uid

--- script.py
+++ script.py
@@ -5,33 +5,33 @@ import time
 
 passw = input("passw: ")
 username = input("username: ")
-id = "100053264810540"
+friend_name = input("friend full name: ")
+
 client = fbchat.Client(username, passw)
-# friends = client.searchForUsers("Bogdan Dovzhnyi")
-# friend = friends[0]
-print(friend.uid)
+friends = client.searchForUsers(friend_name)
+friend = friends[0]
 
 while True:
     try:
-        messages = client.fetchThreadMessages(thread_id=id, limit=20)
+        messages = client.fetchThreadMessages(thread_id=friend.uid, limit=20)
     except:
         print("disconnected, retrying...")
         time.sleep(60)
         client = fbchat.Client(username, passw)
-        messages = client.fetchThreadMessages(thread_id=id, limit=20)
+        messages = client.fetchThreadMessages(thread_id=friend.uid, limit=20)
 
     for message in messages:
-        if message.author != id:
+        if message.author != friend.uid:
             break
-        # elif "🖕" in message.text and message.author == id:
-        elif message.author == id:
+        # elif "🖕" in message.text and message.author == friend.uid:
+        elif message.author == friend.uid:
             try:
-                sent = client.send(fbchat.models.Message("🖕"), id)
+                sent = client.send(fbchat.models.Message("🖕"), friend.uid)
             except:
                 print("disconnected, retrying...")
                 time.sleep(60)
                 client = fbchat.Client(username, passw)
-                sent = client.send(fbchat.models.Message("🖕"), id)
+                sent = client.send(fbchat.models.Message("🖕"), friend.uid)
 
             if sent:
                 print("Message sent successfully!")
